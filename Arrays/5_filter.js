2df6a6c4ad07595d8111068090485a96b847336d
saurabh-gawade-29 <39211222+saurabh-gawade-29@users.noreply.github.com>
2022-03-22 10:08:48 +0530

    filter and some basic txt about DOM

--- Arrays/5_filter.js
+++ Arrays/5_filter.js
@@ -1,14 +1,12 @@
 //! *** TOO IMP*** filter()
-//* Show all satisfied value 
+//* Show all satisfied value
 //* better than find method
 //* also return a callback function
 
 const price = [100, 200, 300, 400, 500, 600];
 
-const newVal = price.filter(
-    (element)=>{
-        return element < 400; 
-    }
-)
+const newVal = price.filter((element) => {
+  return element < 400;
+});
 
 console.log(newVal);

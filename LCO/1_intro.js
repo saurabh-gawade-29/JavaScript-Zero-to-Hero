b16a871fe04aa31c40cb483a228b646b052fdccd
Saurabh Gawade <saurabh.g@finacus.co.in>
2022-04-19 15:00:34 +0530

    LCO basic intro

---
+++ LCO/1_intro.js
@@ -0,0 +1,14 @@
+//** */ JS => Javascript
+
+// TODO: What are JS Engine?
+//* Run the code
+//* V8: GOOGLE - C++
+//* SpiderMoney
+//? Runtime: Deno , Node
+
+// TODO: Which version is good for US?
+//? OLD => HTML+CSS+JS
+//* ECMASCRIPT: ES6 we need to use 2015
+//* Need to learn JS From Basic to ES6
+
+// !GITHUB: KANGAX.github.io
---
+++ LCO/2_variable.js
@@ -0,0 +1,13 @@
+// var, let, const
+var name = "Saurabh Gawade";
+console.log(name);
+
+//TODO: Should not start with upperCase but why?
+//* for better practice and classes are start with Uppercase
+
+// CamelCase: saurabhGawade
+// snakeCase: saurabh_gawade
+
+var paymentMode;
+console.log(paymentMode);
+// ! you will get undefined for above log

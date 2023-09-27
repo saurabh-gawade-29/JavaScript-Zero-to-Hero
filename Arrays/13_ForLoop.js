840fc26278e916af94031517c0f129b4a73a387c
saurabh gawade <saurabhgawade@finacus.co.in>
2023-09-27 15:39:48 +0530

    update

---
+++ Arrays/13_ForLoop.js
@@ -0,0 +1,8 @@
+let myFamily= ["saurabh", "komal", "pradeep", "pratiksha"]
+
+for (i=0; i < myFamily.length; i++){
+    console.log(i, "index" ,myFamily[i], "Value")
+}
+
+// For in Loop: you will get index of array
+// For of Loop: you will get value of array

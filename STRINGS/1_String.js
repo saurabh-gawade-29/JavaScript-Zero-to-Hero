814788257542d30a097e754c5dbc3bd51204630a
saurabh gawade <saurabhgawade@finacus.co.in>
2023-09-27 18:42:06 +0530

    update

---
+++ STRINGS/1_String.js
@@ -0,0 +1,16 @@
+// JS String storing and Manipulate string
+// Double and single Quotes accepted
+
+
+let myName = "Saurabh";
+console.log("🚀 ~ file: 1_String.js:6 ~ myName:", myName)
+//🚀 ~ file: 1_String.js:6 ~ myName: Saurabh
+
+let myChannelName = 'DBurger';
+console.log("🚀 ~ file: 1_String.js:8 ~ myChannelName:", myChannelName)
+// 🚀 ~ file: 1_String.js:8 ~ myChannelName: DBurger
+// Using Prototype
+
+let myNameAgain = new String("Saurabh") // Using Constructor
+console.log("🚀 ~ file: 1_String.js:11 ~ myNameAgain:", myNameAgain)
+// 🚀 ~ file: 1_String.js:11 ~ myNameAgain: [String: 'Saurabh']
---
+++ STRINGS/2_StringMethods.js
@@ -0,0 +1,13 @@
+//! Length: is property and it's count empty space as well
+let myName = "Saurabh Gawade";
+console.log(myName.length);
+
+//! Find String in a String
+console.log(myName.indexOf("Gawade"))
+//Output: 8 Index Value of G
+
+//! if the Value not present in String it will return -1
+console.log(myName.indexOf("test"))
+// Output: -1 ==> test not present here thats why it will return -1
+
+
